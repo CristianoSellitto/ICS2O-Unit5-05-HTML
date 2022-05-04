93e1fc7186b46b5fd696d6e6f61df8dc097f277a
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-05-04 16:45:26 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit5-05-HTML
+// May 4 2022
 
 'use strict'
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-05-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-05-HTML/",
   })
 }
 
 /**
- * Alerts "Hello, World!"
+ * Finds if the triangle is equilateral, isosceles, or scalene depending on the lengths the user input.
  */
 function onButtonClick() {
-  alert("Hello, World!")
+  const side1 = document.getElementById("side1").value
+  const side2 = document.getElementById("side2").value
+  const side3 = document.getElementById("side3").value
+  const allsidesmatch = Boolean(side1 == side2 && side2 == side3)
+  const twosidesmatch = Boolean(side1 == side2 && side2 != side3 || side2 == side3 && side3 != side1 || side1 == side3 && side3 != side2)
+
+  if (allsidesmatch == true) {
+    document.getElementById("result").innerHTML = "<h5>This triangle is equilateral.</h5>"
+  } else if (twosidesmatch == true) {
+    document.getElementById("result").innerHTML = "<h5>This triangle is isosceles.</h5>"
+  } else {
+    document.getElementById("result").innerHTML = "<h5>This triangle is scalene.</h5>"
+  }
 }
